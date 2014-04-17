86d64f42ebc2396cce785ae25e0aadf8ccb1f69a
fernando <fernando@portatil-fer>
2014-04-17 12:02:07 +0200

    Primer commit

---
+++ ProyectoPruebaGitProject/src/main/java/es/dinamicarea/git/prueba/HolaMundo.java
@@ -0,0 +1,12 @@
+package es.dinamicarea.git.prueba;
+
+public class HolaMundo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hola mundo!");
+	}
+
+}
